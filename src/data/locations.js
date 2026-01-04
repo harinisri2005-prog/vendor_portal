571902ef871dca5c0aa229fec8b80b5d7053ddec
harinisri2005-prog <hharinisrirajasekar@gmail.com>
2026-01-04 22:03:11 +0530

    Delete src/data directory

--- src/data/locations.js
+++
@@ -1,7 +0,0 @@
-export const locations= {
-  "Tamil Nadu": ["Chennai", "Coimbatore", "Madurai", "Trichy"],
-  "Karnataka": ["Bengaluru", "Mysuru", "Mangaluru"],
-  "Kerala": ["Kochi", "Trivandrum", "Kozhikode"],
-  "Maharashtra": ["Mumbai", "Pune", "Nagpur"],
-  "Delhi": ["New Delhi"]
-};
